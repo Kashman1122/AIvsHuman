ecab15d83e0e5081cddbb7bf6574c4f1dafec84d
Kashish <134233339+Kashman1122@users.noreply.github.com>
2024-10-02 20:18:17 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,61 @@
 # from nltk.corpus import stopwords
 # import nltk
 # from nltk.stem.porter import PorterStemmer
+
+# ps = PorterStemmer()
+
+# nltk.download('punkt_tab')
+# nltk.download('stopwords')
+# def transform_text(text):
+#     text = text.lower()
+#     text = nltk.word_tokenize(text)
+
+#     y = []
+#     for i in text:
+#         if i.isalnum():
+#             y.append(i)
+
+#     text = y[:]
+#     y.clear()
+
+#     for i in text:
+#         if i not in stopwords.words('english') and i not in string.punctuation:
+#             y.append(i)
+
+#     text = y[:]
+#     y.clear()
+
+#     for i in text:
+#         y.append(ps.stem(i))
+
+#     return " ".join(y)
+
+# tfidf = pickle.load(open('vectorizer.pkl','rb'))
+# model = pickle.load(open('model.pkl','rb'))
+
+# st.title("AI vs Human Detector")
+
+# input_sms = st.text_area("Enter the message")
+
+# if st.button('Predict'):
+
+#     # 1. preprocess
+#     transformed_sms = transform_text(input_sms)
+#     # 2. vectorize
+#     vector_input = tfidf.transform([transformed_sms])
+#     # 3. predict
+#     result = model.predict(vector_input)[0]
+#     # 4. Display
+#     if result == 1:
+#         st.header("Student")
+#     else:
+#         st.header("AI")
+# import streamlit as st
+# import pickle
+# import string
+# from nltk.corpus import stopwords
+# import nltk
+# from nltk.stem.porter import PorterStemmer
 #
 # ps = PorterStemmer()
 #
@@ -85,7 +140,16 @@ tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
 # Title of the app
-st.markdown("<h1 style='text-align: center; color: #ff4d4d;'>AI vs Human Text Detector</h1>", unsafe_allow_html=True)
+st.markdown("""
+    <h1 style="
+        text-align: center; 
+        background: linear-gradient(to right, #ff4d4d, #ff4d4d,#ffa500, #ffffff); 
+        -webkit-background-clip: text;
+        color: transparent;
+    ">
+    AI vs Human Text Detector
+    </h1>
+    """, unsafe_allow_html=True)
 
 # Input box
 input_sms = st.text_area("Enter the message", placeholder="Type here... (at least 10 words)", height=150)
@@ -97,7 +161,7 @@ if input_sms:
 
 # Prediction button
 if st.button('Predict'):
-    if len(input_sms.split()) < 50:
+    if len(input_sms.split()) < 10:
         st.error("Please enter a message with at least 10 words.")
     else:
         # 1. Preprocess the text
